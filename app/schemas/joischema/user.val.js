51a856ed1291210407dc11e42614a6ae8e1ab786
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-10-29 21:57:47 +0100

    Task: Controller and Schema Fix

--- app/schemas/joischema/user.val.js
+++ app/schemas/joischema/user.val.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const loginval = Joi.object({
   email: Joi.string().required().email(),
-  password: Joi.string().required(),
+  password: Joi.string().required().min(5).max(20),
 });
 
 const registerval = Joi.object({
