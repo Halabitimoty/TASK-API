9b400f756d8ed52ce500ac6088bf92aebd775425
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-11-01 23:13:17 +0100

    Feat: Upload pics

---
+++ app/controllers/utils/uploadprofilepic.js
@@ -0,0 +1,35 @@
+const cloud = require("cloudinary").v2;
+
+const { usercollection } = require("../../schemas/user.schema");
+const { config } = require("../../utilities/cloud");
+
+cloud.config(config);
+
+const uploadprofilepic = async (req, res) => {
+  try {
+    const { filename } = req.file;
+    const { userid } = req.decoded;
+
+    const result = await cloud.uploader.upload("public/" + filename, {
+      folder: "taskpicture",
+    });
+    console.log(result.secure_url);
+
+    const userpics = usercollection.findByIdAndUpdate(userid, {
+      profilepicture: result.secure_url,
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "profile picture upload successfully.",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message || "unable to upload picture",
+      message: "unable to upload picture",
+    });
+  }
+};
+
+module.exports = { uploadprofilepic };
--- app/routes/auth.route.js
+++ app/routes/auth.route.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+const upload = multer({ dest: "public/" });
 
 const { isuserloggedin } = require("../middlewares/middlewares");
 const {
@@ -9,11 +12,18 @@ const {
   forgetpassword,
   resetpassword,
 } = require("../controllers/auth/auth.controller");
+const { uploadprofilepic } = require("../controllers/utils/uploadprofilepic");
 
 router.post("/login", login);
 router.post("/register", register);
 router.post("/forgetpassword", forgetpassword);
 router.put("/resetpassword", isuserloggedin, resetpassword);
 router.get("/profile", isuserloggedin, profile);
+router.post(
+  "/uploadprofilepic",
+  isuserloggedin,
+  upload.single("taskpicture"),
+  uploadprofilepic
+);
 
 module.exports = router;
